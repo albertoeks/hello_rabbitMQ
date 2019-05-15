cea0f764282bf8e1e53d413956a21297faaf2437
Alberto <eks.alberto@gmail.com>
2019-05-15 18:55:49 +1000

    Add message ack

--- worker.py
+++ worker.py
@@ -12,7 +12,9 @@ def callback(ch, method, properties, body):
     print(" [x] Received %r" % body)
     time.sleep(body.count(b'.'))
     print(" [x] Done")
+    ch.basic_ack(delivery_tag=method.delivery_tag)
 
 
+channel.basic_consume(queue='hello', on_message_callback=callback)
 channel.start_consuming()
 
